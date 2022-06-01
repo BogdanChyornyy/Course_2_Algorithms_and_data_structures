5642229f8f102d14c3de7dabd1e939b411a49a41
BogdanChyornyy <booggiman@mail.ru>
2022-06-01 23:20:41 +0300

    Создан класс, в потором описана работа приложения.

--- Course_2_Algorithms_and_data_structures/Program.cs
+++ Course_2_Algorithms_and_data_structures/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_2_Algorithms_and_data_structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpNum.TestCode();
+        }        
+    }
+}
