f6a90033e13047bf4ad7b2afaf61730cae636a33
BogdanChyornyy <booggiman@mail.ru>
2022-06-16 03:48:01 +0300

    Инициализировал обход вширину, с поиском

--- Course_2_Algorithms_and_data_structures/BinaryTree.cs
+++ Course_2_Algorithms_and_data_structures/BinaryTree.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Course_2_Algorithms_and_data_structures
-{ 
+{
     internal class BinaryTree
     {
         private string value;
@@ -13,11 +13,17 @@ namespace Course_2_Algorithms_and_data_structures
         private BinaryTree left;
         private BinaryTree right;
 
+        public BinaryTree Node; // экземпляр класса "элемент дерева"
+        public string s;
         // Операция вставки.
         public void Insert(string value)
         {
             if (this.value == null)
+            {
                 this.value = value;
+
+            }
+
             else
             {
                 if (this.value.CompareTo(value) == 1)
@@ -25,12 +31,14 @@ namespace Course_2_Algorithms_and_data_structures
                     if (left == null)
                         this.left = new BinaryTree();
                     left.Insert(value);
+
                 }
                 else if (this.value.CompareTo(value) == -1)
                 {
                     if (right == null)
                         this.right = new BinaryTree();
                     right.Insert(value);
+
                 }
                 else
                     throw new Exception("Узел уже существует");
@@ -112,11 +120,72 @@ namespace Course_2_Algorithms_and_data_structures
             t.Insert("груша");
             t.Insert("яблоко");
             t.Insert("клубника");
+            t.Insert("малина");
+            t.Insert("виноград");
+            t.Insert("личи");
+            t.Insert("апельсин");
+            t.Insert("манго");
+            //Console.WriteLine(t.Display(t));
+            //BinaryTree s = t.Search("мандарин");
+            //Console.WriteLine(s.Display(s));
+            //Console.Read();
+            string somestring = "";
+            bool check = true;
+            Across(t, ref somestring, check, "груша");
+        }
+        /// <summary>
+        /// обход дерева в ширину
+        /// </summary>
+        /// <param name="node"></текущий "элемент дерева".>
+        /// <param name="s"></строка, в которой накапливается результат.>
+        /// <param name="stopWord"></искомое слово.>            
+        private static void Across(BinaryTree node, ref string s, bool detailed, string stopWord)
+        {
+            var queue = new Queue<BinaryTree>(); // создать новую очередь
 
-            Console.WriteLine(t.Display(t));
-            BinaryTree s = t.Search("мандарин");
-            Console.WriteLine(s.Display(s));
-            Console.Read();
+            if (detailed)
+            {
+                s += "    заносим в очередь значение " + node.value.ToString() + Environment.NewLine; queue.Enqueue(node); // поместить в очередь первый уровень                
+            }
+
+            while (queue.Count != 0) // пока очередь не пуста
+            {
+                //если у текущей ветви есть листья, их тоже добавить в очередь
+                if (queue.Peek().left != null)
+                {
+                    if (detailed)
+                    {
+                        s += "    заносим в очередь значение *" + queue.Peek().left.value.ToString() + "* из левого поддерева" + Environment.NewLine;
+                    }
+                    queue.Enqueue(queue.Peek().left);
+                }
+                if (queue.Peek().right != null)
+                {
+                    if (detailed)
+                    {
+                        s += "    заносим в очередь значение *" + queue.Peek().right.value.ToString() + "* из правого поддерева" + Environment.NewLine;
+                    }
+                    queue.Enqueue(queue.Peek().right);
+                }
+                // извлечь из очереди информационное поле последнего элемента
+                if (detailed)
+                {
+                    s += "    извлекаем значение из очереди: " + queue.Peek().value.ToString() + Environment.NewLine;
+
+                    if (queue.Peek().value.ToString() == stopWord)
+                    {
+                        break;
+                    }
+                }
+
+                else
+                {
+                    s += queue.Peek().value.ToString() + " "; // убрать последний элемент очереди
+                }
+
+                queue.Dequeue();
+            }
+            Console.WriteLine(s);
         }
     }
 }
